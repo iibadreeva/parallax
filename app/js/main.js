915382a5d752c2ad813af61050c078a3fa9f007a
iibadreeva <iibadreeva@mail.ru>
2018-07-16 04:30:32 +0300

    add three parallax

--- app/js/main.js
+++ app/js/main.js
@@ -84,6 +84,32 @@ var grass = (function () {
     }
 }());
 
+var moveLayers = (function () {
+    var parallaxContainer = document.querySelector('.parallax__inner'),
+        layers = parallaxContainer.children;
+
+    return {
+        init: function (e) {
+            var initialX = (window.innerWidth / 2) - e.pageX,
+            initialY = (window.innerHeight / 2) - e.pageY;
+
+            [].slice.call(layers).forEach(function (layer, i) {
+                var
+                    divider = i / 200,
+                    positionX = initialX * divider,
+                    positionY = initialY * divider,
+                    // bottomPosition = (window.innerHeight / 2) * divider,
+                    layerStyle = layer.style,
+                    transformString = 'translate3d(' + positionX + 'px ,' + positionY + 'px , 0)';
+
+                layerStyle.transform = transformString;
+                // layerStyle.bottom = '-' + bottomPosition + 'px';
+                // parallaxContainer.style.transform = `translateY(${Math.abs(10)}px)`
+            });
+        }
+    }
+}());
+
 function getCoords(elem) {
     var box = elem.getBoundingClientRect();
 
@@ -189,3 +215,7 @@ window.addEventListener('resize', function () {
 
     grass.init(width);
 })
+
+window.addEventListener('mousemove', function (e) {
+    moveLayers.init(e)
+});
